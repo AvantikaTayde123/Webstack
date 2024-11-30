244c00584e24a522ae2cc6750b987a7c2f451d0a
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-11-30 21:07:50 +0530

    Add files via upload

---
+++ views/First.js
@@ -0,0 +1,36 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.use((req, res) => {
+  res.render("404 Not Found");
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server started on localhost:3000");
+});
